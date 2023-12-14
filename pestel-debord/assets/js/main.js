3d3205ea3b2e1705934841e38fb0b392e40f8201
rambuvn <vn.nqhung@gmail.com>
2023-12-14 09:50:15 +0700

    upate html for pestel debord

--- pestel-debord/assets/js/main.js
+++ pestel-debord/assets/js/main.js
@@ -1,6 +1,30 @@
 (() => {
 	"use strict";
 
+	const readFileAsBase64 = (file) => {
+    return new Promise((resolve, reject) => {
+        // Create a new FileReader object
+        const reader = new FileReader();
+
+        // Listen for the load event
+        reader.addEventListener('load', function() {
+            // The file's content will be in reader.result
+            const base64String = reader.result.replace('data:', '')
+                .replace(/^.+,/, '');
+
+            resolve(base64String);
+        });
+
+        // Listen for the error event
+        reader.addEventListener('error', function() {
+            reject(reader.error);
+        });
+
+        // Read the file as a data URL
+        reader.readAsDataURL(file);
+    });
+}
+
 	document.addEventListener("click", (event) => {
 		const target = event.target;
 		if ( target.classList.contains('btn-option-select')) {
@@ -17,7 +41,7 @@
 			activeOption.classList.add('active');
 			const input = activeOption.querySelector('input');
 			if ( input ) {
-				input.addAttribute('checked', 'checked');
+				input.setAttribute('checked', 'checked');
 				input.checked = true;
 			}
 		}
@@ -33,4 +57,31 @@
 			}
 		}
 	})
+
+	// Submit form handle
+	const processForm = async (formData) => {
+		// get form data
+		const passportUUID = formData.get('lot-number').replace(/\.0+$/,'').replace(".0", "-").replace(".", "-");
+		let storedData = {
+			uuid: passportUUID,
+			client: {
+				name: formData.get('name'),
+				firtname: formData.get('firstname'),
+				email: formData.get('email'),
+				phone: formData.get('tel'),
+			},
+			file: await readFileAsBase64(formData.get('bordereau')),
+			plan: formData.get('pricing-option'),
+		}
+		console.log(storedData);
+
+	}
+	const form = document.querySelector('#pestel-submittion');
+	if ( form ) {
+		form.addEventListener('submit', async (event) => {
+			event.preventDefault();
+			const formData = new FormData(form);
+			processForm(formData);
+		});
+	}
 })();
